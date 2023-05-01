84ded46f151e06f18fdec6bcfd543ff4477fe077
SamuelRG23032 <srg.2001.sg@gmail.com>
2023-05-01 17:41:37 +0100

    bottombar

--- mobile/app/src/main/java/com/example/pint_mobile/pages/PerfilUtilizadorActivity.kt
+++ mobile/app/src/main/java/com/example/pint_mobile/pages/PerfilUtilizadorActivity.kt
@@ -7,13 +7,13 @@ import android.text.Layout
 import android.view.View
 import android.widget.RelativeLayout
 import android.widget.TextView
+import android.widget.Toast
 import com.example.pint_mobile.MainActivity
 import com.example.pint_mobile.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class PerfilUtilizadorActivity : AppCompatActivity() {
 
-    val nav = findViewById<BottomNavigationView>(R.id.bottombar)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_perfil_utilizador)
@@ -21,19 +21,34 @@ class PerfilUtilizadorActivity : AppCompatActivity() {
         supportActionBar?.title = "Perfil"
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        val nav = findViewById<BottomNavigationView>(R.id.bottombar)
+
         nav.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.inicio -> {
-                    // Lidar com ação de favoritos
+                    Toast.makeText(applicationContext, "inicio", Toast.LENGTH_SHORT).show()
+                    true
                 }
                 R.id.noticacao -> {
-                    // Lidar com ação de agendas
+                    Toast.makeText(applicationContext, "notificacao", Toast.LENGTH_SHORT).show()
+                    true
                 }
                 R.id.mais -> {
-                    // Lidar com ação de música
+                    /*val buttonShow = findViewById<ButtonShow>(R.id.buttonShow)
+                    buttonShow.setOnClickListener {
+                        val bottomSheetDialog = BottomSheetDialog(this@MainActivity, R.style.BottomSheetDialogTheme)
+                        val bottomSheetView = layoutInflater.inflate(R.layout.layout_bottom_sheet, findViewById<LinearLayout>(R.id.bottomSheetContainer))
+                        bottomSheetView.findViewById<Button>(R.id.buttonShare).setOnClickListener {
+                            Toast.makeText(this@MainActivity, "Share", Toast.LENGTH_SHORT).show()
+                            bottomSheetDialog.dismiss()
+                        }
+                        bottomSheetDialog.setContentView(bottomSheetView)
+                        bottomSheetDialog.show()
+                    }*/
+                    //precisa mudanças
                 }
+                else -> false
             }
-            true
         }
     }
 
