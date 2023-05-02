0d515c7dc6023946d6762990331997ed472eb71e
SamuelRG23032 <srg.2001.sg@gmail.com>
2023-05-02 23:19:24 +0100

    FFFFFFFFFF

--- mobile/app/src/main/java/com/example/pint_mobile/pages/PerfilUtilizadorActivity.kt
+++ mobile/app/src/main/java/com/example/pint_mobile/pages/PerfilUtilizadorActivity.kt
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Layout
 import android.view.View
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import com.example.pint_mobile.MainActivity
 import com.example.pint_mobile.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+
 class PerfilUtilizadorActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +26,7 @@ class PerfilUtilizadorActivity : AppCompatActivity() {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
         val nav = findViewById<BottomNavigationView>(R.id.bottombar)
+dasdasd
 
         nav.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
@@ -35,23 +39,11 @@ class PerfilUtilizadorActivity : AppCompatActivity() {
                     true
                 }
                 R.id.mais -> {
-                    /*val buttonShow = findViewById<ButtonShow>(R.id.buttonShow)
-                    buttonShow.setOnClickListener {
-                        val bottomSheetDialog = BottomSheetDialog(this@MainActivity, R.style.BottomSheetDialogTheme)
-                        val bottomSheetView = layoutInflater.inflate(R.layout.layout_bottom_sheet, findViewById<LinearLayout>(R.id.bottomSheetContainer))
-                        bottomSheetView.findViewById<Button>(R.id.buttonShare).setOnClickListener {
-                            Toast.makeText(this@MainActivity, "Share", Toast.LENGTH_SHORT).show()
-                            bottomSheetDialog.dismiss()
-                        }
-                        bottomSheetDialog.setContentView(bottomSheetView)
-                        bottomSheetDialog.show()
-                    }*/
-                    //precisa mudanças
+
                 }
                 else -> false
             }
         }
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
