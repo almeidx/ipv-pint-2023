f24b375313ba23444e30c07b88f505a4d52e7ad5
jpedro058 <joao_pedro_12345@hotmail.com>
2023-05-17 11:13:39 +0100

    mobile: admin reuniao

--- mobile/app/src/main/java/com/example/pint_mobile/MainActivity.kt
+++ mobile/app/src/main/java/com/example/pint_mobile/MainActivity.kt
@@ -1,23 +1,20 @@
 package com.example.pint_mobile
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatDelegate
 import com.example.pint_mobile.pages.*
-import com.example.pint_mobile.pages.admin.edit.CriarReuniaoActivity
+import com.example.pint_mobile.utils.ActivityBase
 import com.example.pint_mobile.utils.getCookieValue
 import com.example.pint_mobile.utils.getCurrentUser
 import com.example.pint_mobile.utils.getUserInfo
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ActivityBase(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
 
         val cookie = getCookieValue(this)
         if (cookie != null) {
@@ -35,53 +32,9 @@ class MainActivity : AppCompatActivity() {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 
-    fun gotoNegocios(_view: View) {
-        val intent = Intent(this, NegociosActivity::class.java)
-        startActivity(intent)
-    }
 
     fun gotoLogin(_view: View) {
         val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun gotoSignUp(_view: View) {
-        val intent = Intent(this, SignUpActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun gotoBeneficios(_view: View) {
-        val intent = Intent(this, BeneficiosActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun gotoMudarPassword(_view: View) {
-        val intent = Intent(this, MudarPasswordActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun gotoIdeias(_view: View) {
-        val intent = Intent(this, IdeiasActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun gotoContacto(_view: View) {
-        val intent = Intent(this, ContactoActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun gotoVagas(_view: View) {
-        val intent = Intent(this, VagasActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun gotoPerfil(_view: View){
-        val intent = Intent(this, PerfilActivity::class.java)
-        startActivity(intent)
-    }
-
-    fun gotoNotificacoes(_view: View){
-        val intent = Intent(this, NotificacoesActivity::class.java)
         startActivity(intent)
     }
 
