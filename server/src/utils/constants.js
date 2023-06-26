24980955fb218f8ec2e7dcdf39657b66d0a398b2
Almeida <almeidx@pm.me>
2023-06-26 18:54:17 +0100

    deps

--- server/src/utils/constants.js
+++ server/src/utils/constants.js
@@ -2,6 +2,6 @@ const { randomString } = require("./randomString.js");
 
 exports.ISO_DATETIME_REGEX = /\d{4}-[01]\d-[0-3]\dT[0-2]\d:[0-5]\d:[0-5]\d\.\d+([+-][0-2]\d:[0-5]\d|Z)/;
 
-exports.PASSWORD_RESET_TOKEN = process.env.PASSWORD_RESET_TOKEN + randomString(16);
-exports.MOBILE_GOOGLE_AUTH_TOKEN = process.env.PASSWORD_RESET_TOKEN + randomString(16);
-exports.MOBILE_FACEBOOK_AUTH_TOKEN = process.env.PASSWORD_RESET_TOKEN + randomString(16);
+exports.PASSWORD_RESET_TOKEN = randomString(256);
+exports.MOBILE_GOOGLE_AUTH_TOKEN = randomString(256);
+exports.MOBILE_FACEBOOK_AUTH_TOKEN = randomString(256);
