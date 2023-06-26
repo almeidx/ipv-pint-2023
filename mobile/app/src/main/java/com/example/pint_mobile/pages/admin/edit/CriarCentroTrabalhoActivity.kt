c1410ac450f733b213630201f014ea9244c5c9ae
jpedro058 <98242911+jpedro058@users.noreply.github.com>
2023-06-26 14:42:09 +0100

    Mobile fix (#66)
    * mobile: centroTrabalho
    
    * mobile: fix areaNegocio

---
+++ mobile/app/src/main/java/com/example/pint_mobile/pages/admin/edit/CriarCentroTrabalhoActivity.kt
@@ -0,0 +1,49 @@
+package com.example.pint_mobile.pages.admin.edit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.example.pint_mobile.R
+import com.example.pint_mobile.utils.ActivityBase
+import com.example.pint_mobile.utils.createCentroTrabalho
+import com.example.pint_mobile.utils.createTipoProjeto
+
+class CriarCentroTrabalhoActivity : ActivityBase(R.layout.activity_criar_centro_trabalho, "Criar Centro de Trabalho "){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    fun criarCentroTrabalho(view: View){
+        val nome = findViewById<android.widget.EditText>(R.id.nomeCentroTrabalhoEditText)
+        val location = findViewById<android.widget.EditText>(R.id.locationCentroTrabalhoEditText)
+        val address = findViewById<android.widget.EditText>(R.id.addressCentroTrabalhoEditText)
+        val postalCode = findViewById<android.widget.EditText>(R.id.postalCodeCentroTrabalhoEditText)
+
+        val nomeString = nome.text.toString()
+        val locationString = location.text.toString()
+        val addressString = address.text.toString()
+        val postalCodeString = postalCode.text.toString()
+
+        if (nomeString.isEmpty()) {
+            nome.setBackgroundResource(R.drawable.edittext_red_border)
+            Toast.makeText(this, "Nome não pode ser vazio", Toast.LENGTH_SHORT).show()
+            return
+        }else if (locationString.isEmpty()) {
+            location.setBackgroundResource(R.drawable.edittext_red_border)
+            Toast.makeText(this, "Localização não pode ser vazia", Toast.LENGTH_SHORT).show()
+            return
+        }else if (addressString.isEmpty()) {
+            address.setBackgroundResource(R.drawable.edittext_red_border)
+            Toast.makeText(this, "Morada não pode ser vazia", Toast.LENGTH_SHORT).show()
+            return
+        }   else if (postalCodeString.isEmpty()) {
+            postalCode.setBackgroundResource(R.drawable.edittext_red_border)
+            Toast.makeText(this, "Código Postal não pode ser vazio", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        createCentroTrabalho(nomeString, locationString, addressString, postalCodeString, this)
+    }
+}
--- mobile/app/src/main/java/com/example/pint_mobile/utils/CentroTrabalho.kt
+++ mobile/app/src/main/java/com/example/pint_mobile/utils/CentroTrabalho.kt
@@ -1,4 +1,4 @@
 package com.example.pint_mobile.utils
 
-class CentroTrabalho ( var id: Int, var nome: String ) {
+class CentroTrabalho ( var id: Int, var nome: String, var address: String, var location: String, var postalCode: String  ) {
 }
